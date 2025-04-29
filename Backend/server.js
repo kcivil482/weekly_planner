941ca7b091fe4bc33afc82e66a4c910ce7f9beed
Katya Civil <kcivil482@gmail.com>
2025-04-28 22:07:24 -0400

    mongoDB connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import taskRoutes from "./Routes/task.routes"
+import mongoose from "mongoose"
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
 
 app.use(express.json());
 app.use("/task",taskRoutes);
@@ -15,4 +18,11 @@ app.get("/",(req,res)=>{
 
 app.listen(PORT,()=>{  
     console.log("server running")
+    mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
 });
